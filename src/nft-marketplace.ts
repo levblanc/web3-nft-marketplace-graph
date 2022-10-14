5c84aaa16f96dd956e9b4784f9d5a113787306af
levblanc <levblanc@gmail.com>
2022-10-14 15:53:55 +0800

    Update `handleItemBought` function

--- src/nft-marketplace.ts
+++ src/nft-marketplace.ts
@@ -1,55 +1,40 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Address } from '@graphprotocol/graph-ts';
 import {
-  NFTMarketplace,
+  ItemBought as ItemBoughtEvent,
+  ItemCanceled as ItemCanceledEvent,
+  ItemListed as ItemListedEvent,
+} from '../generated/NFTMarketplace/NFTMarketplace';
+import {
+  ActiveItem,
+  ItemListed,
   ItemBought,
   ItemCanceled,
-  ItemListed
-} from "../generated/NFTMarketplace/NFTMarketplace"
-import { ExampleEntity } from "../generated/schema"
+} from '../generated/schema';
 
-export function handleItemBought(event: ItemBought): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleItemBought(event: ItemBoughtEvent): void {
+  const { buyer, tokenId, nftAddress } = event.params;
+  const itemId = getIdFromEventParams(tokenId, nftAddress);
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  let itemBought = ItemBought.load(itemId);
+  let activeItem = ActiveItem.load(itemId);
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if (!itemBought) {
+    itemBought = new ItemBought(itemId);
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  itemBought.buyer = buyer;
+  itemBought.nftAddress = nftAddress;
+  itemBought.tokenId = tokenId;
+  itemBought.save();
 
-  // Entity fields can be set based on event parameters
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.getEarnings(...)
-  // - contract.getListingItem(...)
+  activeItem!.buyer = buyer;
+  activeItem!.save();
 }
 
-export function handleItemCanceled(event: ItemCanceled): void {}
+export function handleItemCanceled(event: ItemCanceledEvent): void {}
 
-export function handleItemListed(event: ItemListed): void {}
+export function handleItemListed(event: ItemListedEvent): void {}
+
+const getIdFromEventParams = (tokenId: BigInt, nftAddress: Address): string => {
+  return tokenId.toHexString() + nftAddress.toHexString();
+};
